62a5b7670c1848f8373c799ee219cbcf2804ffaf
Juha Airaksinen <juha.airaksinen1@gmail.com>
2021-03-22 15:19:58 +0200

    changed to correct tags

--- src/components/contacts.jsx
+++ src/components/contacts.jsx
@@ -2,8 +2,8 @@ import React from "react"
 export function Contacts() {
     return (
         <h3>
-            <a href="https://gitlab.com/juha_airaksinen">Gitlab</a><t> | </t>
-            <a href="https://github.com/Zerkath">Github</a><t> | </t>
+            <a href="https://gitlab.com/juha_airaksinen">Gitlab</a><text> | </text>
+            <a href="https://github.com/Zerkath">Github</a><text> | </text>
             <a href="https://www.linkedin.com/in/juha-airaksinen-7391241a6/">LinkedIn</a>
         </h3>
     );
